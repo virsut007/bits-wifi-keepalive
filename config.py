9e50fd18d1fc708dc959c94a9cb60784cd8c4da7
virsut007 <virensuthar0207@gmail.com>
2026-03-01 00:26:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py — Edit this file. DO NOT commit to GitHub (it's gitignored).
 
-USERNAME      = "F20251154"         # your BITS ID
-PASSWORD      = "your_password"     # your WiFi password
+USERNAME      = "F20XXXXXX"         # add your BITS ID
+PASSWORD      = "your_password"     # add your WiFi password
 INTERVAL      = 13000               # seconds between keepalive pings
 
 # Paste your current keepalive URL from the browser here.
 # Looks like: https://fw.bits-pilani.ac.in:8090/keepalive?xxxxxxxxxxxxxxxx
 # Leave empty ("") if you want the script to login fresh from scratch.
-KEEPALIVE_URL = "https://fw.bits-pilani.ac.in:8090/keepalive?0b0a020503080509"
+KEEPALIVE_URL = "https://fw.bits-pilani.ac.in:8090/keepalive?urcode" #add ur url
